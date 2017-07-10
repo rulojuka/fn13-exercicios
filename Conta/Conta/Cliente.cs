7121dd600534cee56c72a0a3c8eea72d0276d5a0
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2017-07-10 13:32:19 -0300

    Adiciona novo projeto para o Capitulo 7 - Modificadores de acesso.

---
+++ Conta/Conta/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta
+{
+	class Cliente
+	{
+		public string nome;
+		public string rg;
+		public string cpf;
+		public string endereco;
+		public int idade;
+
+		public bool ehMaiorDeIdade()
+		{
+			return idade >= 18;
+		}
+	}
+}
---
+++ Conta/Conta/Conta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta
+{
+	class Conta
+	{
+		public double Saldo { get; private set; }
+		public Cliente titular;
+		public int numero;
+
+		public void Deposita(double valor)
+		{
+			this.Saldo += valor;
+		}
+
+		public bool Saca(double valor)
+		{
+			if (valor <= this.Saldo)
+			{
+				this.Saldo -= valor;
+				return true;
+			}
+			return false;
+		}
+
+		public void Transfere(double valor, Conta destino)
+		{
+			this.Saca(valor);
+			destino.Deposita(valor);
+		}
+
+	}
+}
